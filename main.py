afb193eba8de26c86baf961ee4f4fd18eb7719ca
Gabriel Tapizquent <gaboth22@gmail.com>
2018-04-04 11:22:22 -0400

    Stop serving file locally. Move all to one thread. Gotta ctrl + z to terminate

--- main.py
+++ main.py
@@ -5,22 +5,10 @@ import YoutubeLivePlaylist as ylp
 import VideoServer as vs
 import os
 import sys
-import signal
 import shutil
 import time
 from threading import Thread
 from threading import Event
-
-def serve_video(args):
-   port = 8000
-
-   if(len(args) == 4):
-      port = int(args[3])
-
-   server.initialize_on_port(port)
-
-   while(not kill_event.is_set()):
-      server.handle_requests()
 
 def download_video_chunks(args):
    youtube_link = args[1]
@@ -34,17 +22,20 @@ def download_video_chunks(args):
    os.makedirs(video_output_path)
    video_number = 0
 
-   while(not kill_event.is_set()):
-      updated_list, live_stream_chunk_urls = playlist.get_current_playlist_urls(live_stream_url)
+   while(True):
+      try:
+         updated_list, live_stream_chunk_urls = playlist.get_current_playlist_urls(live_stream_url)
 
-      if(updated_list):
-         for chunk_url in live_stream_chunk_urls:
-            video_name_with_path = video_output_path + '/' + str(video_number)
-            youtube.download_video_chunk_as_mp4(video_name_with_path, chunk_url)
-            video_number = video_number + 1
-      else:
-         time.sleep(2.5)
-         print("Waiting for updated list...")
+         if(updated_list):
+            for chunk_url in live_stream_chunk_urls:
+               video_name_with_path = video_output_path + '/' + str(video_number)
+               youtube.download_video_chunk_as_mp4(video_name_with_path, chunk_url)
+               video_number = video_number + 1
+         else:
+            time.sleep(2.5)
+            print("Waiting for updated list...")
+      except:
+         pass
 
 def main(cmd_args):
    if('help' in cmd_args[1] or cmd_args[1] == '-h'):
@@ -53,28 +44,12 @@ def main(cmd_args):
    if(len(cmd_args) < 3):
       print "Must provide youtube video URL and video size"
       return
-
-   video_download_thread = Thread(target = download_video_chunks, args = (cmd_args, ))
-   video_server_thread = Thread(target = serve_video, args = (cmd_args, ))
-   video_download_thread.start()
-   video_server_thread.start()
-   video_download_thread.join()
-   video_server_thread.join()
-
-def exit_program(signum, frame):
-   signal.signal(signal.SIGINT, original_sigint)
-   server.close()
-   kill_event.set()
-   shutil.rmtree('video')
-   sys.exit(0)
+   
+   download_video_chunks(cmd_args)
 
 if __name__ == '__main__':
-   kill_event = Event()
-   server = vs.VideoServer()
    youtube = yt.Youtube()
    playlist = ylp.YoutubeLivePlaylist()
-   original_sigint = signal.getsignal(signal.SIGINT)
-   signal.signal(signal.SIGINT, exit_program) 
    try: 
       main(sys.argv)
    except:
