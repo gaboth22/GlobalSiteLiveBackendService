a9f881274ab59f51e5333f037142ce2cd12f3ffd
Gabriel Tapizquent <gaboth22@gmail.com>
2018-04-20 00:38:46 -0400

    Add setting for path to serve

--- ServeVideo.py
+++ ServeVideo.py
@@ -1,12 +1,14 @@
 import SimpleHTTPServer
 import SocketServer
 import sys
+import os
 
-def main(port):
+def main(path_to_serve, port):
+    os.chdir(path_to_serve)
     handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("", port), handler)
     print "Serving on port: ", port
     httpd.serve_forever()
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]))
+    main(sys.argv[1], int(sys.argv[2]))
